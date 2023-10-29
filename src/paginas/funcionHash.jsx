d4b19aa39d3a5b7ade5ecee2540b886bba07b6b3
Andres <andresf150@gmail.com>
2023-10-29 13:09:09 -0600

    Paso 2

--- src/paginas/funcionHash.jsx
+++ src/paginas/funcionHash.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SHA256 } from 'crypto-js';
 
 export default function FuncionHash() {
+  const [inputText, setInputText] = useState('');
+  const [hashResult, setHashResult] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  }
+
+  const calculateHash = () => {
+    const hash = SHA256(inputText).toString();
+    setHashResult(hash);
+  }
+
   return (
-    <h1>Funcion Hash</h1>
+    <div>
+      <h1>Funcion Hash</h1>
+      <input type="text" onChange={handleInputChange} />
+      <button onClick={calculateHash}>Calcular Hash</button>
+      <p>Resultado del Hash: {hashResult}</p>
+    </div>
   );
 }
